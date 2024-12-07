52b080f22f26bf5a4ae04ce4d250e303274f3313
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-12-08 01:30:12 +0600

    added the react reveal package and implemented it

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -4,7 +4,7 @@ import EquipmentCard from "../components/EquipmentCard/EquipmentCard";
 import SportsCategories from "../components/SportsCategories/SportsCategories";
 import TopDeals from "../components/TopDeals/TopDeals";
 import SportsTips from "../components/SportsTips/SportsTips";
-
+import { Bounce, Fade } from "react-awesome-reveal";
 
 const HomeLayout = () => {
     const equipments = useLoaderData();
@@ -17,8 +17,10 @@ const HomeLayout = () => {
                 </section>
                 {/* PRODUCTION SECTION  */}
                 <section className="mt-20">
-                    <h1 className="text-3xl md:text-5xl font-bold text-center mb-5">Our Products</h1>
-                    <p className="text-center mt-2 mb-4 w-4/5 mx-auto">Elevate your game with our premium sports equipment, designed for athletes of all levels. From outdoor adventures to professional training, we offer durable, high-performance gear to help you achieve your goals.</p>
+                    <Bounce cascade><h1 className="text-3xl md:text-5xl font-bold text-center mb-5">Our Products</h1>
+                        <p className="text-center mt-2 mb-4 w-4/5 mx-auto">Elevate your game with our premium sports equipment, designed for athletes of all levels. From outdoor adventures to professional training, we offer durable, high-performance gear to help you achieve your goals.</p>
+                    </Bounce>
+
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-6">
                         {
                             equipments.map(equipment =>
@@ -36,7 +38,9 @@ const HomeLayout = () => {
                 </section>
 
                 <section className="my-10">
-                    <h1 className="text-3xl text-gray-400 font-bold text-center">FIND OUT MORE ...</h1>
+                    <Fade>
+                        <h1 className="text-3xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-red-700 via-purple-300 to-indigo-500">FIND OUT MORE ...</h1>
+                    </Fade>
                 </section>
 
                 <section>
